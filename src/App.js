1ed829084db70538a235c4875554e105bf58807c
Alex Gomes <alex.gomes.net3@gmail.com>
2020-05-27 15:25:22 -0300

    Building the GET repositories route and adding your return to visualization

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    console.log('Adicionar');
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remove
+            </button>
+          </li>
+        )}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Add</button>
     </div>
   );
 }
